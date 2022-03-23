4535f300c00cbd981979c39494e4d673f2263c82
杨胜荣 <yangshengrong@yangsheongdeAir.home>
2022-03-23 22:05:05 +0100

    Update Menu.java // twee methodes (if methode en switch methode) voor doorklikken bij de menu

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -19,14 +19,82 @@ public class Menu {
         System.out.println("0) Exit");
         System.out.println("Uw keuze:");
         int input = scanner.nextInt();
-        if (input == 1) {
+
+        //if statement voor invoeren van de cijfers
+        /* if (input == 1) {
+
             examens();
+        }
+        else if(input == 2){
+            System.out.println("2) Lijst met studenten");
+        }
+        else if(input == 3){
+            System.out.println("3) Nieuwe student inschrijven");
+        }
+        else if(input == 4){
+            System.out.println("4) Student verwijderen");
+        }
+        else if(input == 5){
+            System.out.println("5) Examen afnemen");
+        }
+        else if(input == 6){
+            System.out.println("6) Is student geslaagd voor test?");
+        }
+        else if(input == 7){
+            System.out.println("7) Welke examens heeft student gehaald?");
+        }
+        else if(input == 8){
+            System.out.println("8) Welke student heeft de meeste examens gehaald?");
+        }
+        else if(input == 0){
+            System.out.println("0) Exit");
+        }
+        else {
+            System.out.println("Geen correcte invoer,probeer opnieuw.");
+        }
+    }*/
+        // switch statement voor invoeren van de cijfers
+        switch (input) {
+            case 1:
+                System.out.println("1) Lijst met examens");
+                ;
+                break;
 
-        } 
+            case 2:
+                System.out.println("2) Lijst met studenten");
+                break;
 
+            case 3:
+                System.out.println("3) Nieuwe student inschrijven");
+                break;
 
+            case 4:
+                System.out.println("4) Student verwijderen");
+                break;
 
+            case 5:
+                System.out.println("5) Examen afnemen");
+                break;
+
+            case 6:
+                System.out.println("6) Is student geslaagd voor test?");
+                break;
+
+            case 7:
+                System.out.println("7) Welke examens heeft student gehaald?");
+                break;
+
+            case 8:
+                System.out.println("8) Welke student heeft de meeste examens gehaald?");
+                break;
+
+            case 0:
+                System.out.println("0) Exit");
+                break;
+        }
     }
+
+
 
     private void examens() {
         String[] examen = {"Verkeersexamen_1", "Verkeersexamen_2"};
