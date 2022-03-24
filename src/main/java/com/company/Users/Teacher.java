bc071703e9be66649514d2bc4eb32dfcdd1f7087
huska <huska@MSI.local>
2022-03-24 23:02:57 +0100

    Update Teacher.java

--- src/main/java/com/company/Users/Teacher.java
+++ src/main/java/com/company/Users/Teacher.java
@@ -1,14 +1,67 @@
 package com.company.Users;
+
+import com.company.Menu;
+
+import java.sql.SQLOutput;
 import java.util.ArrayList;
-public class Teacher extends User{
+import java.util.Scanner;
+
+public class Teacher extends User {
     static private ArrayList<Teacher> AlleTeachers;
+
     Teacher(String name, Integer identificatieCode) {
         this.name = name;
         this.identificatieCode = identificatieCode;
     }
-    static private ArrayList <Teacher> GeefTeachersTerug(){
+
+    static private ArrayList<Teacher> GeefTeachersTerug() {
         return AlleTeachers;
     }
-    //
+
+    public static void deleteStudent() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1) Zoek student op naam.");
+        System.out.println("2) Zoek student op identificatiecode.");
+        System.out.println("0) Exit");
+        int input = scanner.nextInt();
+        if (input == 1) {
+            System.out.println("Voer de naam van de student in:");
+            String studentName = scanner.nextLine();
+        } else if (input == 2) {
+            System.out.println("Voer de identificatiecode van de student in:");
+            Integer studentID = scanner.nextInt();
+        } else if (input == 0) {
+            return;
+        } else {
+            System.out.println("Unexpected error. Please check www.hhs.nl for more info.");
+        }
+    }
+
+    public static void generateStudent() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wat is de naam van de student?");
+        String studentName = scanner.nextLine();
+        System.out.println("Wat is de identificatiecode van de student?");
+        Integer studentID = scanner.nextInt();
+        System.out.println("New student name: " + studentName + ", New student ID: " + studentID);
+        System.out.println("1) Accepteer nieuwe student.");
+        System.out.println("2) Probeer opnieuw.");
+        System.out.println("0) Exit.");
+        int input = scanner.nextInt();
+        if(input == 1){
+            System.out.println("Nieuwe student geaccepteerd.");
+            return;
+        }else if(input == 2){
+            generateStudent();
+        }else if(input == 0){
+            return;
+        }else{
+            System.out.println("Unexpected error. Please check www.hhs.nl for more info.");
+        }
+    }
+
 
 }
+
+//
+
