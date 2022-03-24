2af73dd98e48de12515e82cbdb72ee4f004e9b9b
tommy <tommykoning@live.nl>
2022-03-24 19:50:20 +0100

    Update Main.java: made a simple load and save for a students

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -2,21 +2,59 @@ package com.company;
 
 import com.company.Users.Student;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.File;
+import java.io.Reader;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
+        Main.init();
+
         new Menu();
 
+        Main.shutdown();
+    }
+
+    private static void init() {
+        try {
+            // create Gson instance
+            Gson gson = new Gson();
+
+            // create a reader
+            Reader reader = Files.newBufferedReader(Paths.get("src/main/resources/json/students.json"));
+
+            // convert JSON string to User object
+            List<Student> users = new Gson().fromJson(reader, new TypeToken<List<Student>>() {}.getType());
+
+            // print user object
+            users.forEach((Student) -> System.out.println(Student.getName()));
+
+            // close reader
+            reader.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void shutdown() {
         try {
             // create Student object
-            Student floor = new Student("floor", 1);
+            List<Student> users = Arrays.asList(
+                    new Student("floor", 1),
+                    new Student("alexander", 2),
+                    new Student("shuang", 3),
+                    new Student("waihong", 4),
+                    new Student("tommy", 5)
+            );
 
             // create Gson instance
             Gson gson = new Gson();
@@ -31,7 +69,7 @@ public class Main {
             Writer writer = Files.newBufferedWriter(Paths.get("src/main/resources/json/students.json"));
 
             // convert book object to JSON file
-            gson.toJson(floor, writer);
+            gson.toJson(users, writer);
 
             // close writer
             writer.close();
