bd5526f2cfd446af03109d0b484961866c7f5dac
Wai hong <dragon23121412@gmail.com>
2022-03-24 15:07:23 +0100

    Update Exam.java: Uitbreiding exam class

--- src/main/java/com/company/Tests/Exam.java
+++ src/main/java/com/company/Tests/Exam.java
@@ -1,4 +1,46 @@
 package com.company.Tests;
 
+import com.company.Users.Student;
+import com.company.Users.Teacher;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import com.company.Users.Student;
+
 public class Exam {
+
+    static ArrayList<Exam> allExamens;
+    private ArrayList<Question> Vragen;
+    private String uniekeNaam;
+    private Student student;
+    private Teacher teacher;
+    private String vak;
+    private Integer cijferGehaald;
+
+   private Exam (String uniekeNaam,Teacher teacher, Student student, String vak) {
+       this.uniekeNaam = uniekeNaam;
+       this.teacher = teacher;
+       this.student = student;
+       this.vak = vak;
+
+
+        }
+    public void save(){
+    allExamens.add(this);
+
+        }
+    public void nakijken{
+
+
+        }
+
+    public void Cijferchecken(Integer cijferGehaald){
+
+    }
 }
+
+
+
+
+
+
