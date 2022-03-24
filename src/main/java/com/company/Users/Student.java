1a4e3707703092fb48b706b1e26722037059e8d3
31637 <31637@DESKTOP-71IF32S.local>
2022-03-24 14:58:20 +0100

    Update Student.java and User.java: arraylist aangemaakt voor studenten.

--- src/main/java/com/company/Users/Student.java
+++ src/main/java/com/company/Users/Student.java
@@ -1,5 +1,10 @@
 package com.company.Users;
-
+import java.util.ArrayList;
 public class Student extends User{
-    //
+    static private ArrayList<Student> AlleStudenten;
+     Student(String name,Integer identificatieCode ){
+         this.name = name;
+         this.identificatieCode= identificatieCode;
+     }
+
 }
--- src/main/java/com/company/Users/User.java
+++ src/main/java/com/company/Users/User.java
@@ -1,5 +1,6 @@
 package com.company.Users;
 
 public class User {
-    private String name;
+    protected String name;
+    protected Integer identificatieCode;
 }
