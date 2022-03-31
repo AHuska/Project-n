1505d7e5e39348370ab71c0857ab7b335e613321
Wai hong <dragon23121412@gmail.com>
2022-03-31 13:23:38 +0200

    Update Exam.java4

--- src/main/java/com/company/Tests/Exam.java
+++ src/main/java/com/company/Tests/Exam.java
@@ -3,12 +3,11 @@ package com.company.Tests;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import com.company.Users.Student;
-import com.company.Users.Teacher;
+import com.company.Users.*;
 import java.util.Scanner;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
-import com.company.Users.Student;
+import com.company.Users.Student1;
 import java.io.*;
 
 
@@ -17,12 +16,12 @@ public class Exam {
     static ArrayList<Exam> allExamens;
     private ArrayList<Question> Vragen;
     private String uniekeNaam;
-    private Student student;
+    private Student1 student;
     private Teacher teacher;
     private String vak;
     private Integer cijferGehaald;
 
-    private Exam(String uniekeNaam, Teacher teacher, Student student, String vak) {
+    private Exam(String uniekeNaam, Teacher teacher, Student1 student, String vak) {
         this.uniekeNaam = uniekeNaam;
         this.teacher = teacher;
         this.student = student;
@@ -30,18 +29,39 @@ public class Exam {
 
 
     }
-
     static void aanmaken() {
         System.out.println("Wat voor examen wil je?");
         Scanner input = new Scanner(System.in);
-        System.out.print
+        input.nextLine();
+
+
+
 
     }
+    public void initalizearray(){
+        ArrayList<Exam> initialize = new ArrayList<Exam>(){
+            
+
+
+        };
+
+
+    }
+
+
+
+
 
     public void save() {
         allExamens.add(this);
 
     }
+
+
+    public static ArrayList<Exam> getAllExamens() {
+        return  allExamens;
+    }
+
 
 
     public String Cijferchecken(Integer cijferGehaald) {
@@ -51,8 +71,8 @@ public class Exam {
             return "Gezakt";
         }
     }
- }
+  }
 
 
-}
+
     
