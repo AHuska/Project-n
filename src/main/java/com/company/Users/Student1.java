3eaf186f975a491aa1a52fd484b9630b01878997
杨胜荣 <yangshengrong@yangsheongdeAir.home>
2022-04-13 22:04:49 +0200

    Update Student1.java and User.java :contructor en twee getters gemaakt in User; een constructor bij Student1.

--- src/main/java/com/company/Users/Student1.java
+++ src/main/java/com/company/Users/Student1.java
@@ -1,6 +1,9 @@
 package com.company.Users;
 
 public class Student1 extends User  {
-    
 
+
+    public Student1(String name, Integer identificatieCode) {
+        super(name, identificatieCode);
+    }
 }
--- src/main/java/com/company/Users/User.java
+++ src/main/java/com/company/Users/User.java
@@ -4,9 +4,19 @@ public class User {
     protected String name;
     protected Integer identificatieCode;
 
+    public User(String name, Integer identificatieCode){
+        this.name = name;
+        this.identificatieCode = identificatieCode;
+
+    }
+
     public String getName(){
+
         return name;
     }
 
+    public Integer getIdentificatieCode() {
+        return identificatieCode;
+    }
 }
 
